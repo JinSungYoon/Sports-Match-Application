b737fe215d9ba762028f63eaa3d1a04593e17736
JinSungYoon <yjs445566@naver.com>
2022-08-02 21:33:31 +0900

    Change main source code

--- src/main/java/core/SportsMatchApplication.java
+++ src/main/java/core/SportsMatchApplication.java
@@ -1,20 +1,17 @@
 package core;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan("core")
-@EnableJpaAuditing
-@EntityScan(
-		basePackageClasses = {Jsr310JpaConverters.class},
-		basePackages = {"core"})
-@EnableJpaRepositories(basePackages = {"core"})
+@EnableJpaAuditing		// JPA를 사용하면서 AuditingEntityListener 사용하기 위한 어노테이션 추가
 public class SportsMatchApplication {
 
 	public static void main(String[] args) {
